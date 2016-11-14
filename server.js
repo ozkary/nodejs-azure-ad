6fa0cca1c5c0b0dca7eb6f4a6433a77ffacf1d42
ogarcia <ogarcia@magicleap.ds>
2016-11-13 20:40:54 -0500

    read cookie

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 'use strict';
+/*!
+    * Copyright 2016 ozkary.com
+    * http://ozkary.com/ by Oscar Garcia
+    * Licensed under the MIT license. Please see LICENSE for more information.
+    *
+    * ozkary.authtoken
+    * azure ad authentication
+    * ozkary.com
+    * ver. 1.0.0
+    *
+    * Created By oscar garcia 
+    *
+    * Update/Fix History
+    *   ogarcia 10/01/2016 initial implementation
+    *
+    */
 /*
  * require modules
  */
