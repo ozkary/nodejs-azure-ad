fb995e313abb867ba1528992fce46f471a69c425
ogarcia <ogarcia@magicleap.ds>
2016-11-26 11:26:40 -0500

    l branch commit t 

--- modules/auth.js
+++ modules/auth.js
@@ -2,5 +2,8 @@ module.exports.init = function (app) {
     
     var azureOAuth = 'azure_ad_oauth2';
     var appServiceAuth = 'appServiceAuthSession';
-    
+
+    function auth(){
+        
+    }
 };
