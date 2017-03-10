a2b1109b01a5df3565efc91248ba03b37d7871c4
ogarcia <ogarcia@magicleap.ds>
2017-03-10 16:26:07 -0500

    update the user api

--- modules/api/user-api.js
+++ modules/api/user-api.js
@@ -1,15 +1,25 @@
 module.exports.init = function (app) {
     
+    //secured api routes with no redirect
+    function authorizeApi (req, res, next) {
+        var auth = req.isAuthenticated();
+      
+        if (req.isAuthenticated()) {
+            return next();
+        }else{
+            res.status(401).send('Not authorized');
+        }
+    }
+   
+    //authorize the routes
+    app.use('/api/user', authorizeApi);
+  
     //add the route handlers
     app.get('/api/user', profile)  
 
     //returns the user profile currently on the session
     function profile(req, resp){
-        var authSession = req.cookies['AppServiceAuthSession'];
-        if (authSession != null) {
-            user = authSession;
-        }
-        resp.json({ session: user });   
+        resp.json({session:req.user});    
     }
  
 };
