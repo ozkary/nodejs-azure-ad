06f58586553ce847bf16902701664574d71e69e0
ogarcia <ogarcia@magicleap.ds>
2017-03-09 22:16:11 -0500

    basic auth validation

--- modules/api/user-api.js
+++ modules/api/user-api.js
@@ -1,25 +1,15 @@
 module.exports.init = function (app) {
     
-    //secured api routes with no redirect
-    function authorizeApi (req, res, next) {
-        var auth = req.isAuthenticated();
-      
-        if (req.isAuthenticated()) {
-            return next();
-        }else{
-            res.status(401).send('Not authorized');
-        }
-    }
-   
-    //authorize the routes
-    app.use('/api/user', authorizeApi);
-  
     //add the route handlers
     app.get('/api/user', profile)  
 
     //returns the user profile currently on the session
     function profile(req, resp){
-        resp.json({session:req.user});    
+        var authSession = req.cookies['AppServiceAuthSession'];
+        if (authSession != null) {
+            user = authSession;
+        }
+        resp.json({ session: user });   
     }
  
 };
