53ca0eae4d526b987279b7ee0cbd440638a0cfca
ogarcia <ogarcia@magicleap.ds>
2017-03-09 21:55:42 -0500

    route changes and server

--- modules/routes.js
+++ modules/routes.js
@@ -16,27 +16,35 @@
     */
 module.exports.init = function (app, __dirname, passport) {
     
-    //authorize the routes   
-    app.use('/login', passport.authorize);
+    //authorize the routes  using passport
+    if (passport){
 
-    //add the route handlers    
+        app.use('/login', passport.authorize);
+
+        //route handler for the post authentication from identity provider
+        app.get('/onauth', passport.login(),
+            function(req,res){
+                login(req,res);   
+            } 
+        ); 
+    
+      
+    }
+    
+    //add the route for login/out handlers    
     app.get('/login', login)
+    app.get('/logout', logout)
 
+    //login
     function login (req, res){
         res.sendFile( __dirname + "/app/index.html");
-    }
-
-    //route handler for the post authentication from identity provider
-    app.get('/onauth', passport.login(),
-        function(req,res){
-            login(req,res);   
-        } 
-    ); 
-  
+    }  
+    
     // logout
-    app.get('/logout', function (req, res) {
+    function logout (req, res) {
         req.logout();
-        res.redirect('/');
-    });
+        req.session.destroy();
+        res.redirect('/');                
+    }
  
 };
